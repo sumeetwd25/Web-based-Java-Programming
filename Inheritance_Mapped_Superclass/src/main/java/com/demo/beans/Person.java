70043a8047f33f6c11c6bc96eeb15df05c9712c9
Sumeet Wadile <sumeetwd25@gmail.com>
2023-06-30 16:48:04 +0530

    Add files via upload

---
+++ Inheritance_Mapped_Superclass/src/main/java/com/demo/beans/Person.java
@@ -0,0 +1,47 @@
+package com.demo.beans;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+
+
+@MappedSuperclass
+public class Person {
+	@Id
+	private int pid;
+	private String pname;
+	private String address;
+	public Person() {
+		super();
+	}
+	public Person(int pid, String pname, String address) {
+		super();
+		this.pid = pid;
+		this.pname = pname;
+		this.address = address;
+	}
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Person [pid=" + pid + ", pname=" + pname + ", address=" + address + "]";
+	}
+	
+
+}
