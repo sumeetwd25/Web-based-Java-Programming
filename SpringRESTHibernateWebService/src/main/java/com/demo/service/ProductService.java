c2e4a89946f26354af621a04750d55cbe312287a
Sumeet Wadile <sumeetwd25@gmail.com>
2023-07-06 10:09:54 +0530

    Add files via upload

---
+++ SpringRESTHibernateWebService/src/main/java/com/demo/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.demo.service;
+
+import java.util.List;
+
+import com.demo.beans.Product;
+
+public interface ProductService {
+
+	List<Product> getAllProducts();
+
+	Product getById(int pid);
+
+	int addNewProduct(Product p);
+
+	int updateProduct(Product p);
+
+	int deleteById(int pid);
+	
+}
