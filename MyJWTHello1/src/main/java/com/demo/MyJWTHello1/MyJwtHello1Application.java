d35876aee66d8facce6e6524a61c114e85e0046c
Sumeet Wadile <sumeetwd25@gmail.com>
2023-07-08 18:22:41 +0530

    Add files via upload

---
+++ MyJWTHello1/src/main/java/com/demo/MyJWTHello1/MyJwtHello1Application.java
@@ -0,0 +1,13 @@
+package com.demo.MyJWTHello1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJwtHello1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJwtHello1Application.class, args);
+	}
+
+}
---
+++ MyJWTHello1/src/main/java/com/demo/MyJWTHello1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo.MyJWTHello1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello From HelloController.---- .";
+	}
+}
